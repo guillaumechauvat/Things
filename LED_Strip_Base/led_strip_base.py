0e3410a64a651c47be74350ee915046239cd177b
Guillaume Chauvat <chauvat@mech.kth.se>
2022-12-11 16:03:19 +0100

    add a fillet on the wire hole and start making the main box

--- LED_Strip_Base/led_strip_base.py
+++ LED_Strip_Base/led_strip_base.py
@@ -11,11 +11,12 @@ led_height = 4
 base_thickness = 1.5
 base_side_thickness = 1
 base_padding = 5
-top_thickness = 8
+top_thickness = 4
 walls = 1
 fillet = 1
 bottom_fillet = 0.5
 wire_width = 5
+wire_fillet = 0.5
 
 # Calculated parameters
 total_length = acrylic_length + base_padding
@@ -23,6 +24,7 @@ total_width = led_width + 2 * walls
 inner_length = total_length - 2 * walls
 inner_width = led_width
 base_extra_thickness = base_thickness - base_side_thickness
+box_height = led_height + top_thickness + base_extra_thickness
 
 # outer part
 base = (
@@ -51,4 +53,13 @@ base = (
     .transformed(offset=cq.Vector(inner_length / 2, 0, 0))
     .rect(2 * walls, wire_width)
     .extrude(base_extra_thickness)
+    .faces('>Z').edges('>X').fillet(wire_fillet)
+)
+# move lid on the side
+base = base.translate((0, -2 * total_width, 0))
+
+box = (
+    cq.Workplane('XY')
+    .rect(total_length, total_width)
+    .extrude(box_height)
 )
